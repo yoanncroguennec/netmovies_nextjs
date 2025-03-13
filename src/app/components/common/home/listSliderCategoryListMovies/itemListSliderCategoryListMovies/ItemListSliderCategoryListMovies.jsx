58dc57cf8d7a28cebb9acef6b4b9053202241c7f
sido69 <yoann.croguennec@gmail.com>
2025-03-13 14:24:45 +0100

    first commit

--- src/app/components/common/home/listSliderCategoryListMovies/itemListSliderCategoryListMovies/ItemListSliderCategoryListMovies.jsx
+++ src/app/components/common/home/listSliderCategoryListMovies/itemListSliderCategoryListMovies/ItemListSliderCategoryListMovies.jsx
@@ -39,8 +39,8 @@ export default function ItemListSliderCategoryListMovies({ item }) {
   return (
     <Link key={movie._id} href={`/pages/movies/${movie._id}`}>
       <RootItemListSliderCategoryListMovies>
-        <NameMovieItem variant={matches ? "string" : "body2"}>
-          {movie.name} //{movie._id}
+        <NameMovieItem variant={matches ? "string" : "h5"}>
+          {movie.name}
         </NameMovieItem>
         <img alt={movie.name} src={movie.img} style={StylesImgItem} />
       </RootItemListSliderCategoryListMovies>
