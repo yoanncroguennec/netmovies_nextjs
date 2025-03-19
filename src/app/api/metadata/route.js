56463b9da275fe880c8e851120080d110f3c04a1
sido69 <yoann.croguennec@gmail.com>
2025-03-19 13:36:29 +0100

    first commit

--- src/app/api/metadata/route.js
+++ src/app/api/metadata/route.js
@@ -1,38 +1,36 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 
-export default async function handler(req, res) {
+export async function GET() {
   const RADIO_URL = "https://generations.fr/radio/webradio/22/generations-2000";
 
   try {
-    // Récupération du contenu HTML de la page
+    // Récupération du HTML de la page de la radio
     const { data } = await axios.get(RADIO_URL);
-
-    // Chargement du HTML avec cheerio
     const $ = cheerio.load(data);
 
-    // Sélection de la section contenant les informations des webradios
-    const webradiosSection = $(
-      'section:contains("En ce moment sur nos webradios")'
-    );
-
-    // Parcours des éléments pour trouver Générations 2000
     let artist = "Inconnu";
     let title = "Inconnu";
     let image = "";
 
-    webradiosSection.find("div").each((index, element) => {
-      const radioName = $(element).find("h2").text().trim();
-      if (radioName === "Generations 2000") {
-        const trackInfo = $(element).find("p").text().trim();
-        [artist, title] = trackInfo.split(" - ");
-        image = $(element).find("img").attr("src");
+    // Rechercher la section contenant les titres en cours
+    $('section:contains("En ce moment sur nos webradios") div').each(
+      (index, element) => {
+        const radioName = $(element).find("h2").text().trim();
+        if (radioName === "Generations 2000") {
+          const trackInfo = $(element).find("p").text().trim();
+          [artist, title] = trackInfo.split(" - ");
+          image = $(element).find("img").attr("src");
+        }
       }
-    });
+    );
 
-    res.status(200).json({ artist, title, image });
+    return Response.json({ artist, title, image });
   } catch (error) {
     console.error("Erreur lors de la récupération des métadonnées:", error);
-    res.status(500).json({ error: "Impossible de récupérer les métadonnées" });
+    return Response.json(
+      { error: "Impossible de récupérer les métadonnées" },
+      { status: 500 }
+    );
   }
 }
