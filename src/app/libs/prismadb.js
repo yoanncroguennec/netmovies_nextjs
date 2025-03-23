e32971b19ab03c761b7e015280fbec2e3043a788
sido69 <yoann.croguennec@gmail.com>
2025-03-23 21:32:12 +0100

    first commit

--- src/app/libs/prismadb.js
+++ src/app/libs/prismadb.js
@@ -1,9 +1,17 @@
-import { PrismaClient } from "@prisma/client"
+// import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+// export default prisma;
+
+// // const client = globalThis.prisma || new PrismaClient()
+// // if (process.env.NODE_ENV !== "production") globalThis.prisma = client
+
+// // export default client
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
+
 export default prisma;
-
-// const client = globalThis.prisma || new PrismaClient()
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = client
-
-// export default client
