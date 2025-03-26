60c6ca010001b7f82f2898032a5eb70227154dca
sido69 <yoann.croguennec@gmail.com>
2025-03-26 17:06:00 +0100

    first commit

--- src/app/api/webradios/[id]/route.ts
+++ src/app/api/webradios/[id]/route.ts
@@ -5,7 +5,36 @@ export async function DELETE(
   req: Request,
   { params }: { params: { id: string } }
 ) {
-  const { id } = params;
-  await prisma.webRadio.delete({ where: { id } });
-  return NextResponse.json({ message: "Deleted" }, { status: 200 });
+  try {
+    const { id } = params;
+    await prisma.webRadio.delete({ where: { id } });
+
+    return NextResponse.json(
+      { message: "Deleted successfully" },
+      {
+        status: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to delete WebRadio" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
 }
--- src/app/api/webradios/route.ts
+++ src/app/api/webradios/route.ts
@@ -1,9 +1,17 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 
+const allowedOrigins = ["http://localhost:3000", "https://www.net-movie.fr/"];
+
 export async function GET() {
   const radios = await prisma.webRadio.findMany();
-  return NextResponse.json(radios);
+  return NextResponse.json(radios, {
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Change "*" to specific origins if needed
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
 }
 
 export async function POST(req: Request) {
@@ -12,8 +20,27 @@ export async function POST(req: Request) {
     const newRadio = await prisma.webRadio.create({
       data: { name, img, fluxUrl },
     });
-    return NextResponse.json(newRadio, { status: 201 });
+
+    return NextResponse.json(newRadio, {
+      status: 201,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
   } catch (error) {
     return NextResponse.json({ error: "Error creating post" }, { status: 500 });
   }
+}
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
 }
