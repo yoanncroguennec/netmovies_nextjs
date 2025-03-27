379f016efb5cc9e384f55b787116b32cca594b04
sido69 <yoann.croguennec@gmail.com>
2025-03-27 21:01:06 +0100

    first commit

--- src/app/pages/admin/movies/newMovie/page.tsx
+++ src/app/pages/admin/movies/newMovie/page.tsx
@@ -1,33 +1,77 @@
 "use client";
 
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useRouter } from "next/navigation";
 import Container_Admin from "@/app/components/layouts/containers/container_Admin/Container_Admin";
-import { Box, TextField, Typography } from "@mui/material";
-import axios from "axios";
+// API
+import { fetchMovies } from "./api";
+// ICONS
 import { MdDelete } from "react-icons/md";
 import { HiPlus } from "react-icons/hi";
+import {
+  Box,
+  Checkbox,
+  FormControl,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
 
-export default function NewWebRadioPage() {
+const formControl = {
+  backgroundColor: "#000",
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#F00",
+    },
+    "&:hover fieldset": {
+      borderColor: "#F00",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#F00",
+    },
+    "& input": {
+      color: "red",
+    },
+  },
+  "& .MuiInputLabel-root": {
+    color: "#F00",
+  },
+  "& .MuiInputLabel-root.Mui-focused": {
+    color: "red",
+  },
+  color: "white",
+  margin: "0 10px",
+};
+
+export default function AddMoviePage() {
   const router = useRouter(); // Initialize useRouter
 
+  const [loading, setLoading] = useState<boolean>(false);
+  const [country, setCountry] = useState<string[]>([]);
+  const [genres, setGenres] = useState<string[]>([]);
+  const [selectedCountry, setSelectedCountry] = useState<string[]>([]);
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
+// console.log('====================================');
+// console.log(selectedCountry);
+// console.log('====================================');
   const [formData, setFormData] = useState({
     name: "",
     realisators: [""],
-    actors: [""],
+    actors: "",
     desc: "",
     trailer: "",
-    favorite: false,
-    watch: false,
-    country: ["String", "String", "String"],
+    country: "",
     productionCompany: "",
     movieLink: "",
     img: "",
-    year: 1998,
-    genre: ["String", "String", "String"],
-    rating: "",
+    year: "",
+    genre: [""],
   });
 
   const [error, setError] = useState({
@@ -35,10 +79,6 @@ export default function NewWebRadioPage() {
     movieLink: false,
     img: false,
   });
-
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
 
   // Helper function for URL validation
   function validateURL(url: string) {
@@ -70,51 +110,71 @@ export default function NewWebRadioPage() {
     toast.success("Champ de réalisateur supprimé !");
   }
 
-  //
-  function handleAddFieldActors() {
-    setFormData((prev) => ({
-      ...prev,
-      actors: [...prev.actors, ""],
-    }));
-    toast.info("Champ d'acteur ajouté !");
+  useEffect(() => {
+    const getMovies = async () => {
+      setLoading(true);
+      try {
+        const data = await fetchMovies(); // Fetch movies from the external API
+
+        // Extraire et trier les country
+        const uniqueCountry = [
+          ...new Set(data.flatMap((movie) => movie.country)),
+        ].sort((a, b) => a.localeCompare(b));
+        setCountry(uniqueCountry);
+
+        // Extraire et trier les genres
+        const uniqueGenres = [
+          ...new Set(data.flatMap((movie) => movie.genre)),
+        ].sort();
+        setGenres(uniqueGenres); // Mise à jour de l'état genres
+      } catch (err) {
+        toast.error(
+          "Impossible de récupérez la listes des pays et des genres de films de la BDD !"
+        );
+        console.error(err);
+      }
+    };
+
+    getMovies();
+  }, []);
+
+  function handleGenreChange(e: React.ChangeEvent<{ value: unknown }>) {
+    const { value } = e.target;
+    setSelectedGenres(value as string[]);
   }
 
-  function handleChangeActors(index: number, value: string) {
-    setFormData((prev) => {
-      const newActors = [...prev.actors];
-      newActors[index] = value;
-      return { ...prev, actors: newActors };
-    });
+  function handleCountryChange(e: React.ChangeEvent<{ value: unknown }>) {
+    const { value } = e.target;
+    setSelectedCountry(value as string[]);
   }
 
-  function handleDeleteFieldActors(index: number) {
-    setFormData((prev) => ({
-      ...prev,
-      actors: prev.actors.filter((_, i) => i !== index),
-    }));
-    toast.success("Champ d'acteur supprimé !");
-  }
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-  async function handleSubmit(e: React.FormEvent) {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    // Validate fields
-    if (
-      !formData.name ||
-      !formData.realisators ||
-      !formData.actors ||
-      !formData.desc ||
-      !formData.trailer ||
-      !formData.country ||
-      !formData.productionCompany ||
-      !formData.movieLink ||
-      !formData.img ||
-      !formData.year ||
-      !formData.genre
-    ) {
-      toast.error("Veuillez remplir tous les champs obligatoires !");
-      return;
-    }
+    // // Validate fields
+    // if (
+    //   !formData.name ||
+    //   !formData.realisators ||
+    //   !formData.actors ||
+    //   !formData.desc ||
+    //   !formData.trailer ||
+    //   !formData.country ||
+    //   !formData.productionCompany ||
+    //   !formData.movieLink ||
+    //   !formData.img ||
+    //   !formData.year ||
+    //   !formData.genre
+    // ) {
+    //   toast.error("Veuillez remplir tous les champs obligatoires !");
+    //   return;
+    // }
 
     const trailerValid = validateURL(formData.trailer);
     const movieLinkValid = validateURL(formData.movieLink);
@@ -130,42 +190,24 @@ export default function NewWebRadioPage() {
       return;
     }
 
+    const data = {
+      ...formData,
+      actors: formData.actors.split(","), // Méthode .split(",") : divise un string en un array en fonction d'un séparateur spécifique, la c'est une virgule ","
+      // country: formData.country.split(","),
+      country: selectedCountry,
+      genre: selectedGenres,
+      year: parseInt(formData.year, 10), // Function parseInt: converti un string en un integer (INT)
+    };
+
     try {
-      const res = await axios.post(
-        "https://www.net-movie.fr/api/movies",
-        formData,
-        {
-          headers: { "Content-Type": "application/json" },
-        }
-      );
-
-      if (res.status === 200) {
-        toast.success("Film ajouté avec succès !");
-        setFormData({
-          name: "",
-          realisators: [""],
-          actors: [""],
-          desc: "",
-          trailer: "",
-          favorite: false,
-          watch: false,
-          country: [""],
-          productionCompany: "",
-          movieLink: "",
-          img: "",
-          year: 1998,
-          genre: [""],
-          rating: ""
-        });
-
-        // setTimeout(() => {
-        //   router.push("/webradios");
-        // }, 2000);
-      }
+      await axios.post("https://www.net-movie.fr/api/movies", data);
+      toast.success("Film ajouté avec succès !");
+      //  router.push("/pages/admin/movies");
     } catch (error) {
       toast.error("Une erreur est survenue lors de l'ajout du Film.");
+      console.error("Erreur lors de l'ajout du film :", error);
     }
-  }
+  };
 
   return (
     <Container_Admin>
@@ -193,12 +235,18 @@ export default function NewWebRadioPage() {
 
         <form
           onSubmit={handleSubmit}
-          style={{ display: "flex", flexDirection: "column", width: "500px" }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            height: "1000px",
+            width: "500px",
+          }}
         >
           <TextField
             label='Nom du film'
             name='name'
             onChange={handleChange}
+            required
             type='text'
             value={formData.name}
             variant='outlined'
@@ -211,8 +259,11 @@ export default function NewWebRadioPage() {
             >
               <TextField
                 label={`Réalisateur ${index + 1}`}
-                value={realisator}
+                name='realisators'
                 onChange={(e) => handleChangeRealisators(index, e.target.value)}
+                required
+                type='text'
+                value={realisator}
                 fullWidth
               />
               <MdDelete
@@ -230,36 +281,23 @@ export default function NewWebRadioPage() {
             </Box>
           ))}
 
-          {formData.actors.map((actor, index) => (
-            <Box
-              key={index}
-              sx={{ display: "flex", alignItems: "center", mb: 2 }}
-            >
-              <TextField
-                label={`Acteur ${index + 1}`}
-                value={actor}
-                onChange={(e) => handleChangeActors(index, e.target.value)}
-                fullWidth
-              />
-              <MdDelete
-                color='#F00'
-                onClick={() => handleDeleteFieldActors(index)}
-                size={45}
-                style={{ cursor: "pointer", padding: "0 5px" }}
-              />
-              <HiPlus
-                color='#000'
-                onClick={handleAddFieldActors}
-                size={45}
-                style={{ cursor: "pointer", padding: "0 5px" }}
-              />
-            </Box>
-          ))}
+          <TextField
+            label='Acteurs (séparés par des virgules)'
+            name='actors'
+            onChange={handleChange}
+            required
+            type='text'
+            value={formData.actors}
+            variant='outlined'
+          />
 
           <TextField
             label='Description du film'
+            multiline
             name='desc'
             onChange={handleChange}
+            required
+            rows={5}
             type='text'
             value={formData.desc}
             variant='outlined'
@@ -272,9 +310,10 @@ export default function NewWebRadioPage() {
                 ? "Lien invalide, doit commencer par http:// ou https://"
                 : ""
             }
-            label='Photo URL de la webRadio'
+            label='URL Bande-annonce du film'
             name='trailer'
             onChange={handleChange}
+            required
             type='text'
             value={formData.trailer}
             variant='outlined'
@@ -284,6 +323,7 @@ export default function NewWebRadioPage() {
             label='Compagnie de production du film'
             name='productionCompany'
             onChange={handleChange}
+            required
             type='text'
             value={formData.productionCompany}
             variant='outlined'
@@ -296,9 +336,10 @@ export default function NewWebRadioPage() {
                 ? "Lien invalide, doit commencer par http:// ou https://"
                 : ""
             }
-            label='Stream URL'
+            label='URL STREAM du film'
             name='movieLink'
             onChange={handleChange}
+            required
             type='text'
             value={formData.movieLink}
             variant='outlined'
@@ -311,13 +352,48 @@ export default function NewWebRadioPage() {
                 ? "Lien invalide, doit commencer par http:// ou https://"
                 : ""
             }
-            label='Stream URL'
+            label='URL Photo du film'
             name='img'
             onChange={handleChange}
+            required
             type='text'
             value={formData.img}
             variant='outlined'
           />
+
+          <FormControl sx={[formControl, { width: "500px" }]}>
+            <InputLabel>Filtrer par pays</InputLabel>
+            <Select
+              label='Filtrer par pays'
+              multiple
+              MenuProps={{
+                PaperProps: {
+                  style: {
+                    color: "#000",
+                    maxHeight: 300,
+                  },
+                },
+              }}
+              onChange={handleCountryChange}
+              renderValue={(selected) => selected.join(", ")}
+              sx={{
+                color: "#F00",
+                "& .MuiMenuItem-root": {
+                  color: "purple",
+                },
+              }}
+              value={selectedCountry}
+            >
+              {country.map((country_item) => (
+                <MenuItem key={country_item} value={country_item}>
+                  <Checkbox
+                    checked={selectedCountry.indexOf(country_item) > -1}
+                  />
+                  <ListItemText primary={country_item} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
 
           <TextField
             InputProps={{
@@ -337,12 +413,50 @@ export default function NewWebRadioPage() {
             label='Date de sortie du film'
             name='year'
             onChange={handleChange}
+            required
             type='number'
             value={formData.year}
             variant='outlined'
           />
 
-          <button type='submit'>Ajouter nouveau film</button>
+          <FormControl sx={[formControl, { width: "500px" }]}>
+            <InputLabel>Filtrer par genres</InputLabel>
+            <Select
+              label='Filtrer par genres'
+              multiple
+              MenuProps={{
+                PaperProps: {
+                  style: {
+                    color: "#000",
+                    maxHeight: 300,
+                  },
+                },
+              }}
+              onChange={handleGenreChange}
+              renderValue={(selected) => selected.join(", ")}
+              sx={{
+                color: "#F00",
+                "& .MuiMenuItem-root": {
+                  color: "purple",
+                },
+              }}
+              value={selectedGenres}
+            >
+              {genres.map((genre) => (
+                <MenuItem key={genre} value={genre}>
+                  <Checkbox checked={selectedGenres.indexOf(genre) > -1} />
+                  <ListItemText primary={genre} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <button
+            type='submit'
+            className='w-full bg-blue-500 text-white p-2 rounded'
+          >
+            Ajouter
+          </button>
         </form>
       </Box>
     </Container_Admin>
