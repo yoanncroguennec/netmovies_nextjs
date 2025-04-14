b9f02102c768bb01cd7895a49b432c4d68d1ba27
sido69 <yoann.croguennec@gmail.com>
2025-04-14 19:42:59 +0200

    first commit

--- src/app/pages/intro/IntroPage.jsx
+++ src/app/pages/intro/IntroPage.jsx
@@ -1,19 +1,12 @@
-import React from 'react'
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/system";
+import Intro_Desktop from "./desktop/Intro_Desktop";
+import Intro_Cellular from "./cellular/Intro_Cellular";
 
 export default function IntroPage() {
-  return (
-    <div>IntroPage</div>
-  )
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
+    return <div>IntroPage</div>;
+  // return matches ? <Intro_Desktop /> : <Intro_Cellular />
 }
-
-// import { useMediaQuery } from "@mui/material";
-// import { useTheme } from "@mui/system";
-// import Intro_Desktop from "./desktop/Intro_Desktop";
-// import Intro_Cellular from "./cellular/Intro_Cellular";
-
-// export default function IntroPage() {
-//   const theme = useTheme();
-//   const matches = useMediaQuery(theme.breakpoints.up("sm"));
-
-//   return matches ? <Intro_Desktop /> : <Intro_Cellular />
-// }
