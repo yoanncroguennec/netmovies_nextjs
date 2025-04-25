913fbf56443dac64da82df5b10aa2d234e5af3c1
sido69 <yoann.croguennec@gmail.com>
2025-04-25 17:47:44 +0200

    first commit

--- src/app/api/auth/[...nextauth]/[...nextauth].ts
+++ src/app/api/auth/[...nextauth]/[...nextauth].ts
@@ -1,6 +1,6 @@
-// pages/api/auth/[...nextauth].ts
+// // pages/api/auth/[...nextauth].ts
 
-import NextAuth from "next-auth";
-import authOptions from "./authOptions"; // Importation de authOptions depuis le fichier authOptions.ts
+// import NextAuth from "next-auth";
+// import authOptions from "./authOptions"; // Importation de authOptions depuis le fichier authOptions.ts
 
-export default NextAuth(authOptions); // Applique la configuration de NextAuth
+// export default NextAuth(authOptions); // Applique la configuration de NextAuth
--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,60 +1,60 @@
-// pages/api/auth/authOptions.ts
+// // pages/api/auth/authOptions.ts
 
-import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/app/libs/prismadb";
-import { compare } from "bcryptjs";
+// import { NextAuthOptions } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import prisma from "@/app/libs/prismadb";
+// import { compare } from "bcryptjs";
 
-const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "jwt", // Utilisation de la stratégie JWT
-  },
-  providers: [
-    CredentialsProvider({
-      name: "Email et mot de passe",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Mot de passe", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) return null;
+// const authOptions: NextAuthOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   session: {
+//     strategy: "jwt", // Utilisation de la stratégie JWT
+//   },
+//   providers: [
+//     CredentialsProvider({
+//       name: "Email et mot de passe",
+//       credentials: {
+//         email: { label: "Email", type: "email" },
+//         password: { label: "Mot de passe", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         if (!credentials?.email || !credentials?.password) return null;
 
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
+//         const user = await prisma.user.findUnique({
+//           where: { email: credentials.email },
+//         });
 
-        if (!user?.password) return null;
+//         if (!user?.password) return null;
 
-        const isValid = await compare(credentials.password, user.password);
-        if (!isValid) return null;
+//         const isValid = await compare(credentials.password, user.password);
+//         if (!isValid) return null;
 
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-        };
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/login", // Redirection vers la page de connexion
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id; // Ajoute l'ID au token
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (session.user && token.id) {
-        session.user.id = token.id as string; // Ajoute l'ID à la session
-      }
-      return session;
-    },
-  },
-};
+//         return {
+//           id: user.id,
+//           name: user.name,
+//           email: user.email,
+//         };
+//       },
+//     }),
+//   ],
+//   pages: {
+//     signIn: "/login", // Redirection vers la page de connexion
+//   },
+//   callbacks: {
+//     async jwt({ token, user }) {
+//       if (user) {
+//         token.id = user.id; // Ajoute l'ID au token
+//       }
+//       return token;
+//     },
+//     async session({ session, token }) {
+//       if (session.user && token.id) {
+//         session.user.id = token.id as string; // Ajoute l'ID à la session
+//       }
+//       return session;
+//     },
+//   },
+// };
 
-export default authOptions; // Exportation par défaut de la configuration
+// export default authOptions; // Exportation par défaut de la configuration
