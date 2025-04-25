b6a00f019984060ff4e02a0873034f452c93ae88
sido69 <yoann.croguennec@gmail.com>
2025-04-25 18:01:34 +0200

    first commit

--- src/app/components/layouts/customs/navbar/InfosUser.jsx
+++ src/app/components/layouts/customs/navbar/InfosUser.jsx
@@ -1,14 +1,14 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/libs/authOptions";
+// import { authOptions } from "@/app/libs/authOptions";
 import { Typography } from "@mui/material";
 
 export default async function InfosUser() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
   return (
     <div>
       <Typography sx={{ color: "#FFF", fontWeight: "bold" }} variant='h6'>
-        LOPPPP /// {session?.user?.email} ----
+        {/* LOPPPP /// {session?.user?.email} ---- */}
       </Typography>
     </div>
   );
--- src/app/pages/dashboard/page.tsx
+++ src/app/pages/dashboard/page.tsx
@@ -1,21 +1,21 @@
 import { getServerSession } from "next-auth";
-import authOptions from "../../../../bad/[...nextauth]/[...nextauth]/authOptions"; 
+// import authOptions from "../../../../bad/[...nextauth]/[...nextauth]/authOptions"; 
 import { redirect } from "next/navigation";
 import LogoutButton from "./logout-button";
 import Container_GlobalApp from "@/app/components/layouts/containers/container_GlobalApp/Container_GlobalApp";
 import { Box } from "@mui/material";
 
 export default async function DashboardPage() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  if (!session) {
-    redirect("/login");
-  }
+  // if (!session) {
+  //   redirect("/login");
+  // }
 
   return (
     <Container_GlobalApp>
       <Box sx={{ margin: "150px" }}>
-        <h1>Bienvenue, {session.user?.name || session.user?.email} !</h1>
+        {/* <h1>Bienvenue, {session.user?.name || session.user?.email} !</h1> */}
         <p>Voici ton espace personnel.</p>
         <LogoutButton />
       </Box>
